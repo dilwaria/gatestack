e4d223decc7e7244dbe087cff8ad721e5084cfc4
Mukul Dilwaria <mukul.dilwaria@nagarro.com>
2017-11-09 20:40:08 +0530

    desc seo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,6 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
     <meta name="author" content="www.gatestack.com, contact@gatestack.in">
     <meta name="copyright" content="www.gatestack.com"> 
     <title>GateStack | An approach for cracking GATE| Online Institue for GATE</title>
