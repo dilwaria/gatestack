9992696144f443af9653e40e9bd2518e0c4e549e
root <root@mythologger.localdomain>
2018-02-25 17:16:23 +0000

    resolved conflictds

--- public/js/template.js
+++ public/js/template.js
@@ -54,6 +54,9 @@ jQuery(function($) {
 	
 	/* =============== SMOOTH SCROOL EFFECT =============== */
 	$('#nino-navbar ul li a').on('click',function (e) {
+	    if(this.hash.length===0){
+                return;
+            }
 	    e.preventDefault();
 	    var target = this.hash,
 	    $target = $(target);
